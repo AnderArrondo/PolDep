a2b5291871059efcde3af6e9a0c95a0b0045ca2c
Alberto Garcia <garcia.alberto@opendeusto.es>
2025-03-27 20:09:42 +0100

    Creacion/modificacion de las estructutas crimen y criminal

--- modules/crimen.h
+++ modules/crimen.h
@@ -1,8 +1,10 @@
 #ifndef CRIMEN_H
 #define CRIMEN_H
+#include "criminal.h"
 
 typedef struct{
 
+    Criminal *autor;
     char *dni;
     char *descripcion;
     int aino;
--- modules/criminal.h
+++ modules/criminal.h
@@ -1,6 +1,16 @@
 #ifndef CRIMINAL_H
 #define CRIMINAL_H
+#include "crimen.h"
 
+typedef struct{
 
+    char *nombre;
+    char *apelido;
+    int edad;
+    char *genero;
+    char *ciudadNacimiento;
+    Crimen *historialCrimenes;
+    char *estadoCivil;
+} Criminal;
 
 #endif
