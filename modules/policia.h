ca12740523fddea8b94f556731b2f59ce860c453
Diego Razquin Boedo <d.razquin@opendeusto.es>
2025-03-30 12:52:37 +0200

    modificar la estructura de policia

--- modules/policia.h
+++ modules/policia.h
@@ -3,9 +3,9 @@
 #include "policia.h"
 
 typedef struct{
-    char dni[10];
-    char nombre[20];
-    char contrasena[20];
+    char *dni[10];
+    char *nombre[20];
+    char *contrasena[20];
 }Policia;
 
 
