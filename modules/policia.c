eb0ee123c11a274ec0074908944fd17862ec858a
Diego Razquin Boedo <d.razquin@opendeusto.es>
2025-03-30 12:08:22 +0200

    Creacion de la structura de policia

---
+++ modules/policia.c
@@ -0,0 +1,3 @@
+#include "policia.h"
+#include <stdio.h>
+
---
+++ modules/policia.h
@@ -0,0 +1,12 @@
+#ifndef POLICIA_H
+#define POLICIA_H
+#include "policia.h"
+
+typedef struct{
+    char dni[10];
+    char nombre[20];
+    char contrasena[20];
+}Policia;
+
+
+#endif
