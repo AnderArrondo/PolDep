bb1f449619b877cff425b1c94300eb0ba155e1a6
Alberto Garcia <garcia.alberto@opendeusto.es>
2025-04-25 21:26:07 +0200

    creacion de funcion insertNewCrime

---
+++ db/database.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sqlite3.h>
+#include <crimen.h>
+
+int insertNewCrime(sqlite3 *db, Crimen crimen){
+
+    sqlite3_stmt *stmt;
+
+    char sql[] = "Insert into crime (id, nombre, apellido, edad, genero, estadoCivil, ciudadNacimiento, dni, descripcion, anioStr) values (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+
+    if (result != SQLITE_OK) {
+		printf("Error preparing statement (INSERT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+    printf("SQL query prepared (INSERT)\n");
+
+    result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) {
+		printf("Error inserting new data into country table\n");
+		return result;
+	}
+
+    result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (INSERT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("Prepared statement finalized (INSERT)\n");
+
+	return SQLITE_OK;
+}
