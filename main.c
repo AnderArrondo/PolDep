2db39bcf79dd0e5abf2b4ea109e7b4d837b53bfc
pablowin <pablo.perea@opendeusto.es>
2025-04-27 17:32:29 +0200

    menu agregado

--- main.c
+++ main.c
@@ -8,35 +8,6 @@
 #include <stdbool.h>
 
 int main(void) {
-    sqlite3 *db;
-    bool dbcargada = false;
-
-    int result = sqlite3_open("./db/data.sqlite3", &db);
-    if (result != SQLITE_OK) {
-        printf("Error opening database\n");
-        return result;
-    } else {
-        dbcargada = true;
-    }
-
-    if (dbcargada == true) {
-        char *filenames[2] = {
-            "./data/crime_and_incarceration_by_state.csv",
-            "./data/prison_custody_by_state.csv"
-        };
-
-        csv_to_db(filenames, 2, db);
-    }
-
-    // Aquí puedes poner el menú principal
-
-    result = sqlite3_close(db);
-    if (result != SQLITE_OK) {
-        printf("Error closing database\n");
-        printf("%s\n", sqlite3_errmsg(db));
-        return result;
-    }
-
-    return 0;
+menu();
 }
 
